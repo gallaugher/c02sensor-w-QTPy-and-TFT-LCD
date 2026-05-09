d4fd3a171f4f8dd9ea63d569698c8b9a50fbebdd
Gallaugher <john.gallaugher@bc.edu>
2026-05-09 12:40:05 -0400

    Upgrade FourWire import for CircuitPython 10 compatibility
    Updated import statement for FourWire to be compatible with CircuitPython 10 and later.

--- code-for-sgp30-sensor(only approx c02 & voc values).py
+++ code-for-sgp30-sensor(only approx c02 & voc values).py
@@ -9,7 +9,7 @@ import adafruit_sgp30
 import displayio, terminalio
 import storage, supervisor
 from adafruit_st7735r import ST7735R
-from displayio import FourWire
+from fourwire import FourWire # upgraded for CircuitPython 10 and up
 from adafruit_display_text import label
 from adafruit_bitmap_font import bitmap_font
 
